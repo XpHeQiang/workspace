07348597b8fe6cb361a05a030f00c3fd74898676
jiaz <heqxxx@163.com>
2017-09-14 00:15:50 +0800

    还差一个判断是否将原json文件读取完的属性

--- express/server/server.js
+++ express/server/server.js
@@ -1,50 +1,71 @@
 //express_demo.js 文件
 var express = require('express');
+var buf = new Buffer(1024);
 var fs = require('fs');  //文件操作
 var app = express();
  
 app.get('/', function (req, res) {
-  console.log('------------------------------------');
-  console.log(344444);
-  console.log('------------------------------------');
    res.send('Hello World');
 })
 app.get('/aaa', function (req, res) {
   var data = req.query.aa; //接收客户端发送过来的数据
-  data = JSON.parse(data);
+  var last = req.query.last; //接收客户端发送过来的数据
 
-  console.log(data[1])
+  console.log(last)
 
-  var dataArr = [];
-  for(var i=0; i<data[0].length; i++){
-    var dataObj = {
-      name : data[i][1],
-      value : data[i][0],
-      parent : data[i][2]
-    };
-    dataArr.push(dataObj);
-  }
+  // data = JSON.parse(data);
+  // var dataArr = [];
+  // for(var i=0; i<data[0].length; i++){
+  //   var dataObj = {
+  //     name : data[i][1],
+  //     value : data[i][0],
+  //     parent : data[i][2]
+  //   };
+  //   dataArr.push(dataObj);
+  // }
 
-  console.log(dataArr);
+  // if(lsat){
+  //   fs.appendFile('address.json', JSON.stringify(dataArr) ,  function(err) {});
+  // }
 
-  console.log("准备写入文件");
-  fs.writeFile('address.json', JSON.stringify(dataArr) ,  function(err) {
-     if (err) {
-         return console.error(err);
-     }
-     console.log("数据写入成功！");
-     console.log("--------我是分割线-------------")
-     console.log("读取写入的数据！");
-     fs.readFile('address.json', function (err, data) {
-        if (err) {
-           return console.error(err);
-        }
-        console.log("异步读取文件数据: " + data.toString());
-     });
-  });
+  
+
+  // console.log("准备打开已存在的文件！");
+  // fs.open('address.json', 'r+', function(err, fd) {
+  //    if (err) {
+  //        return console.error(err);
+  //        fs.writeFile('address.json', "" ,  function(err) {});
+  //    }
+  //    console.log("文件打开成功！");
+  //    console.log("准备读取文件：");
+  //    fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+  //       if (err){
+  //          console.log(err);
+  //       }
+        
+  //       // 仅输出读取的字节
+  //       if(bytes > 0){
+  //         console.log(1111111)
+  //         var datas = JSON.parse(buf.slice(0, bytes).toString());
+  //         console.log(datas.length);
+  //         for (var i = 0; i < datas.length; i++) {
+  //           dataArr.push(datas[i]);
+  //         }
+  //       };
+
+  //         console.log(222222)
+  //         console.log(dataArr)
+  //         console.log("-----------------------------");
+  //       fs.appendFile('address.json', JSON.stringify(dataArr) ,  function(err) {});
+  //   });
+
+  // })
 
 
 })
+
+
+
  
 var server = app.listen(8081, function () {
  
